acb5d8b55264780e78f8937e2da161c6c24c2751
Ivan <vanyashakhovv@gmail.com>
2022-11-09 11:45:21 +0600

    +

--- src/components/accordeon/Accordeon.js
+++ src/components/accordeon/Accordeon.js
@@ -54,7 +54,6 @@ const Accordeon = ({ setData, ls }) => {
         }
       }
     }
-    console.log(arr);
     const randomData = arr
       .map(function (elem) {
         return [elem, Math.random()];
